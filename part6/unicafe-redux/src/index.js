07041cc002559fec747c40b334e55a77a7d1272f
DiscipleVIII <discipleviii@protonmail.com>
2019-12-19 12:18:40 +0100

    part 6

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+  const total = Object.values(store.getState()).reduce((a, b) => a + b)
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>neutral</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={zero}>reset stats</button>
+
+      <div>good {store.getState().good}</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+
+      <div>total {total}</div>
+
+      <div>positive {(store.getState().good + store.getState().ok) / total}</div>
+      <div>avg {(store.getState().good - store.getState().bad) / total}</div>
+
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
